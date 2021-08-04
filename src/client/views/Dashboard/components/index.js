2394fd5869edfd542bdb43e1ce0a6d883027db99
OmeletWithoutEgg <2qbingxuan@gmail.com>
2021-08-04 23:33:37 +0800

    Remove unused components

--- src/client/views/Dashboard/components/index.js
+++ src/client/views/Dashboard/components/index.js
@@ -1,6 +1,6 @@
 export { default as StreakDisplay } from "./StreakDisplay";
-export { default as LatestOrders } from "./LatestOrders";
-export { default as LatestProducts } from "./LatestProducts";
+// export { default as LatestOrders } from "./LatestOrders";
+// export { default as LatestProducts } from "./LatestProducts";
 export { default as LatestSales } from "./LatestSales";
 export { default as TasksProgress } from "./TasksProgress";
 export { default as TotalProfit } from "./TotalProfit";
