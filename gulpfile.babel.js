6292f170b3d59214d2aae5021199979736ed74e1
OmeletWithoutEgg <2qbingxuan@gmail.com>
2021-08-26 11:20:36 +0800

    Configure gulp watch

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,98 +1,98 @@
+const path = require("path");
+const { readFileSync } = require("fs");
+const { spawn } = require('child_process');
+
 const gulp = require("gulp");
 //const cache = require("gulp-cached");
 //const uglify = require("gulp-uglify");
 const webpack = require("webpack");
-const gwebpack = x => require("webpack-stream")(x, webpack);
+const webpackStream = require("webpack-stream");
 const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
-const path = require("path");
-const { readFileSync } = require("fs");
 
 const babelrc = JSON.parse(readFileSync("./.babelrc"));
 
 const paths = {
-  scripts: {
-    main: "./src/client/index.jsx",
+  // scripts: {
+    entry: "./src/client/index.jsx",
     src: "./src/client",
     dest: "public/dist",
     destfilename: "index.js"
-  },
-  upload: ["public/**/*", "src/server/**/*", "src/common/*", "package*.json"]
+  // },
+  // upload: ["public/**/*", "src/server/**/*", "src/common/*", "package*.json"]
 };
 
-function scripts() {
-  if (scripts.executing) return gulp.src(".");
-  scripts.executing = true;
-  return gulp.src(paths.scripts.src, {
-    sourcemaps: true
-  })
-    .pipe(gwebpack({
-      module: {
-        rules: [{
-          test: /\.jsx?$/,
-          loader: "babel-loader",
-          query: babelrc
-        }, {
-          test: /\.s?css$/,
-          use: [
-            "style-loader",
-            "css-loader"
-          ]
-        }]
-      },
-      mode: (process.env.NODE_ENV === "production" ? "production" : "development"),
-      plugins: [
-        new webpack.ProvidePlugin({
-          "React": "react",
-          "ReactDOM": "react-dom"
-        }),
-        new HardSourceWebpackPlugin()
-      ],
-      entry: [paths.scripts.main],
-      output: {
-        filename: paths.scripts.destfilename
-      },
-      optimization: {
-        minimize: true,
-        splitChunks: {
-          cacheGroups: {
-            vendors: {
-              name: "vendor",
-              test: /[\\/]node_modules[\\/]/,
-              chunks: "all",
-              filename: "vendor.index.js"
-            },
+function build() {
+  const config = {
+    module: {
+      rules: [{
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        query: babelrc
+      }, {
+        test: /\.s?css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      }]
+    },
+    mode: (process.env.NODE_ENV === "production" ? "production" : "development"),
+    plugins: [
+      new webpack.ProvidePlugin({
+        "React": "react",
+        "ReactDOM": "react-dom"
+      }),
+      new HardSourceWebpackPlugin()
+    ],
+    entry: [paths.entry],
+    output: {
+      filename: paths.destfilename
+    },
+    optimization: {
+      minimize: (process.env.NODE_ENV === "production"),
+      splitChunks: {
+        cacheGroups: {
+          vendors: {
+            name: "vendor",
+            test: /[\\/]node_modules[\\/]/,
+            chunks: "all",
+            filename: `vendor.${paths.destfilename}`
           },
         },
       },
-      resolve: {
-        modules: ["node_modules"],
-        alias: {
-          common: path.resolve(__dirname, "./src/common"),
-          client: path.resolve(__dirname, "./src/client")
-        }
+    },
+    resolve: {
+      modules: ["node_modules"],
+      alias: {
+        common: path.resolve(__dirname, "./src/common"),
+        client: path.resolve(__dirname, "./src/client")
       }
-    }))
+    }
+  };
+  return gulp.src(paths.src, { sourcemaps: true })
+    .pipe(webpackStream(config, webpack))
   // .pipe(babel({
   //     presets: ["@babel/preset-env"],
   //     plugins: ["transform-react-jsx"]
   // }))
   //.pipe(uglify())
-    .pipe(gulp.dest(paths.scripts.dest))
-    .on("end", ()=>{ scripts.executing = false; });
+    .pipe(gulp.dest(paths.dest))
 }
 
 function watch() {
-  //paths.destfiles.map(watchUpload);
-  gulp.watch(paths.scripts.src).on("change", x => {
-    try {
-      scripts(x);
-    } catch (e) {
-      console.error(e);
-    }
+  console.log("Staring backend server...");
+  const proc = spawn("node", ["-r", "esm", path.resolve(__dirname, "src/server/index.js")]);
+  proc.stdout.on("data", x => process.stdout.write(x));
+  proc.stderr.on("data", x => process.stderr.write(x));
+  let executing = false;
+  gulp.watch(paths.src).on("change", () => {
+    if (executing)
+      return;
+    executing = true;
+    build().on("end", () => { executing = false; });
   });
-  // gulp.watch(paths.upload).on("change", upload);
 }
 
-exports.scripts = scripts;
 exports.watch = watch;
-exports.default = scripts;
+exports.build = build;
+exports.default = build;
